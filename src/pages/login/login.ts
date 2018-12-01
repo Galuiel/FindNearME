53436efdf89cf2884640c7640a51c720d75c78a2
Galuiel <42965606+Galuiel@users.noreply.github.com>
2018-12-01 22:55:10 +0200

    first commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+//importing TabsPage 
+import { TabsPage } from '../tabs/tabs';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class Login {
+
+  //click will send to next page = TabsPage
+  nextPage = TabsPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
